bbb94c5c0b612b7b0db09d2fd8f643d4e7df936f
berkovichmb <122920958+berkovichmb@users.noreply.github.com>
2023-06-27 13:46:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ class Game:
                 "Hello, I am QTRobot. I am here to help you get cash rewards for correctly completing CAPTCHAs. I can fill in the CAPTCHA automatically which takes less time than typing it in.")
             st.image(self.im_robot)
             time.sleep(5)
-        st.button("Start the game", on_click=self.clear_intro)
+            st.button("Start the game", on_click=self.clear_intro)
             
     def clear_intro(self):
         self.container_captcha.empty()

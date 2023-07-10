df73068d8695bd24329849465436095bef0cf0cd
berkovichmb <122920958+berkovichmb@users.noreply.github.com>
2023-07-10 13:14:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"boris_berkovich@student.uml.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
